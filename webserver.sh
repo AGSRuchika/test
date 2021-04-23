e1dba97beaeb95983c591619272a485db69c7152
AGSRuchika <agsruchika@gmail.com>
2021-04-23 10:42:13 +0000

    Adding a test script

--- webserver.sh
+++ webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install -y httpd
-echo "<h1><This webpage has been built with the help of Jnekins server</h1><body style="background-color:grey;"></body>" > /var/www/html/index.html
+echo "<h1><This webpage has been built with the help of Jenkins server</h1><body>This is a sample page of jenkins</body>" > /var/www/html/index.html
 systemctl start httpd
 systemctl status httpd
 sleep 10
